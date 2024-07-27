d67719b821316efc1a1c4127d8d25e944fdc984f
davistheweb <smartkelvin775@gmail.com>
2024-07-27 16:59:50 +0100

    :

--- Source1.f
+++ Source1.f
@@ -1,5 +1,15 @@
-       program hey
-       print*, "Hello";
+       program EvenNumbers
+       implicit none
+       integer:: num = 1
+       
+       do while( num <= 20)
+          if(mod(num,2) == 0)then
+                print "(i2)", num
+                num = num + 1
+                cycle
+          end if
+          num = num +1
+       end do
        
        pause
-       end program hey
+       end program evenNumbers
